fed5f64c924e521d37638ab950bc4d2c92943a11
alconaut <alconautgh@yandex.com>
2016-10-17 13:40:52 +0600

    Better test coverage of the flags package

--- flags/flags_test.go
+++ flags/flags_test.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/conveyer/xflag/flags"
 )
@@ -11,19 +12,52 @@ import (
 var (
 	nilStrs = flags.String("nil[]", nil, "Nil default strings.")
 
-	defaultNames = []string{"a", "b", "c"}
-	names        = flags.String("name[]", defaultNames, "A list of names")
+	defStrs = []string{"a", "b", "c"}
+	strs    = flags.String("name[]", defStrs, "A list of strings.")
+
+	defInts = []int{1, 2, 3}
+	ints    = flags.Int("int[]", defInts, "A list of ints.")
+
+	defInt64s = []int64{1, 2, 3}
+	int64s    = flags.Int64("int64[]", defInt64s, "A list of int64s.")
+
+	defUints = []uint{1, 2, 3}
+	uints    = flags.Uint("uint[]", defUints, "A list of uints.")
+
+	defUint64s = []uint64{1, 2, 3}
+	uint64s    = flags.Uint64("uint64[]", defUint64s, "A list of uint64s.")
+
+	defFloat64s = []float64{1, 2, 3}
+	float64s    = flags.Float64("float64[]", defFloat64s, "A list of float64s.")
+
+	defBools = []bool{true, false, true}
+	bools    = flags.Bool("bool[]", defBools, "A list of bools.")
+
+	dur, _       = time.ParseDuration("1h5m0s")
+	defDurations = []time.Duration{dur}
+	durations    = flags.Duration("duration[]", defDurations, "A list of durations.")
 )
 
 func TestString_NilDefault(t *testing.T) {
-	if names == nil {
+	if strs == nil {
 		t.Fail()
 	}
 }
 
-func TestString(t *testing.T) {
-	if !reflect.DeepEqual(*names, defaultNames) {
-		t.Fail()
+func TestFuncs(t *testing.T) {
+	for _, v := range [][2]interface{}{
+		{defStrs, *strs},
+		{defInts, *ints},
+		{defInt64s, *int64s},
+		{defUints, *uints},
+		{defUint64s, *uint64s},
+		{defFloat64s, *float64s},
+		{defBools, *bools},
+		{defDurations, *durations},
+	} {
+		if !reflect.DeepEqual(v[0], v[1]) {
+			t.Errorf("Expected `%v`, got `%v`.", v[0], v[1])
+		}
 	}
 }
 
