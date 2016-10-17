db8133285b0b64c607e2f64b45a1403770dcf952
alconaut <alconautgh@yandex.com>
2016-10-17 19:01:01 +0600

    Updated Config interface

--- config/config.go
+++ config/config.go
@@ -1,5 +1,17 @@
 package config
 
+import (
+	"flag"
+)
+
+var (
+	// FlagNameSectSep is what is used for separating sections in flag names.
+	// E.g. there is a flag name "person:firstname". If the separator is ":"
+	// and INI configuration file used, the value associated with key "firstname"
+	// will be extracted from [person] section of the configuration file.
+	FlagNameSectSep = flag.String("flagNameSectSep", ":", "A separator of sections in flag names.")
+)
+
 // Interface describes the methods that must be implemented
 // in order to add support of an arbitrary configuration file format.
 type Interface interface {
@@ -7,18 +19,27 @@ type Interface interface {
 	New() Interface
 
 	// Parse should open and parse the requested configuration file.
-	// It should be possible to call it a few times, so the files
-	// will be joined.
 	Parse(string) error
 
-	// Join gets a body of a new configuration file that must be merged.
-	// The input value must have a priority over current Config values,
-	// i.e. values of the input argument must override the ones of
-	// the Config.
+	// Join should get a new configuration file and merge it with the current one.
+	// Values of the input object have priority over the values of the current configuration.
+	// E.g. if the object we have looks as follows:
+	//	obj:
+	//		key1 = value1
+	//		key2 = value2
+	// and the input configuration is:
+	//	obj:
+	//		key2 = another_value
+	//		key3 = value3
+	// Join must turn the original object into:
+	//	obj:
+	//		key1 = value1
+	//		key2 = another_value
+	//		key3 = value3
 	Join(interface{}) error
 
-	// Get receives an argument name and returns a string value associated
-	// with it as a result. If no values are found, an empty string and false
-	// as a second result are returned.
-	Get(string) (string, bool)
+	// Prepare gets a flag that should be set if a value with appropriate name and
+	// type is presented in the configuration file.
+	// Otherwise, the flag must be ignored.
+	Prepare(*flag.Flag)
 }
